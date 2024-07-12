da242285d02061fafb1f2f69c01e51de6150ce23
ozz1webdev <ozz1webdev@gmail.com>
2024-07-12 22:49:51 +0200

    add serializers

---
+++ backend/project/api/serializers.py
@@ -0,0 +1,12 @@
+from jango.contrib.auth import get_user_model
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(**validated_data)
+        return user
