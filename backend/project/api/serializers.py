9cb971b9abe84963bfa5d308b18c9656282ad5b6
ozz1webdev <ozz1webdev@gmail.com>
2024-07-13 02:32:00 +0200

    adding tokens

--- backend/project/api/serializers.py
+++ backend/project/api/serializers.py
@@ -1,12 +1,14 @@
-from jango.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from rest_framework import serializers
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = get_user_model()
-        fields = ('id', 'username', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
+        model = User
+        fields = ["id", "username", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
-        user = get_user_model().objects.create_user(**validated_data)
+        print(validated_data)
+        user = User.objects.create_user(**validated_data)
         return user
